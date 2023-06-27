6ff034dadbd225aa2604e854c902728e6fdb03c6
Anup Karki <anupkarki2012@gmail.com>
2023-06-27 22:12:17 +0545

    Modify String

--- Basic Course/Strings & Operators/modify_string.py
+++ Basic Course/Strings & Operators/modify_string.py
@@ -1,1 +1,31 @@
-# Here we are learning about how we can modify string in python programming
+# Here we are learning about how we can modify string in python programming Language
+# There are lot's of build in methods which can be used in python String to modify it
+
+# Example of Uppercase
+str1= "Software Engineer"
+res = str1.upper()
+print(res)
+
+# Example of lowercase
+str2 = "SOFTWARE ENGINEER"
+re = str2.lower()
+print(re)
+
+# Example of Removing Whitespace from the string
+str3 = " Python Programming  "
+r = str3.strip()
+print(r)
+
+# Replace String in python with another string
+str4 = "Python is a Language"
+replace = str4.replace("P", "C")
+print(replace)
+
+# Split The string in python 
+str5 = "Hello, Python, Programmer"
+split_str = str5.split(".")
+print(split_str)
+
+
+# There are many other Build in Method for string modification in python thhe usage are same as above example
+# We can learn as per the need and used
