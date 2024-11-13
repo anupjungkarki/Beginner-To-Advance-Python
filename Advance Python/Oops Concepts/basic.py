945ea02b4ae41c76dd031b4e11a3ea8ab3d7712f
anupjungkarki <anupkarki2012@gmail.com>
2024-11-13 15:05:06 +0545

    Basic Opps Example

--- Advance Python/Oops Concepts/basic.py
+++ Advance Python/Oops Concepts/basic.py
@@ -4,18 +4,32 @@
 class Student:
     # Constructor to initialize the attributes 
     def __init__(self, name, age):
+        # instance attribute
         self.name = name 
         self.age = age
 
-    #Method 
+    #Method to get the name 
     def get_name(self):
-        return f"The name of student is {self.name} ."
+        return f"The name of student is {self.name}."
     
     # Method to get age
     def get_age(self):
-        return f"{self.name} is {self.age} year's old . "
+        return f"{self.name} is {self.age} year's old. "
     
-# creating an object  instant of the student class 
+# creating an object instant of the student class 
+stu = Student("Anup", 23)
 
-obj = Student("Anup", 23)
-print(obj.get_name)
+# Access attributes and methods of the object
+print(stu.get_name())
+print(stu.get_age())
+
+
+# Let's explain the code how it works
+'''
+1.Class ( Student): A blueprint for creating objects. It has attributes (name and age) and methods (get_name() and get_age()).
+2.Constructor (__init__): The __init__ method initializes name and age when a Student object is created.
+3.Object (stu): An instance of the Student class, created with the name "Anup" and age 23.
+4.Attributes: Store data related to the object (name and age).
+5.Methods: Define behaviors (get_name() and get_age()) of the object.
+
+'''
